c91a0e74e859483a0f6412d3fd878d80bfd6a82d
Deepika Chauhan <deepikachauhan8988@gmail.com>
2026-06-15 14:40:39 +0530

    updated app and added componets

---
+++ src/components/director_panel/demand_&_distribution_reports/mahila_poshan/DirMahilaPoshanDemandSector.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Card, Spinner } from "react-bootstrap";
+
+// import "../../../assets/css/supervisorleftnav.css";
+
+import DirectorLeftNav from "../../DirectorLeftNav";
+import DirectorHeader from "../../DirectorHeader";
+import { useAuth } from "../../../all_login/AuthContext";
+
+
+const DIRECTOR_DASHBOARD_API = "director/dashboard/";
+
+const DirMahilaPoshanDemandSector = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
+
+  const { api, isReady } = useAuth();
+  const [districtCount, setDistrictCount] = useState(null);
+  const [projectCount, setProjectCount] = useState(null);
+  const [sectorCount, setSectorCount] = useState(null);
+  const [distributionData, setDistributionData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+
+
+  const getDistributionCount = (financialYear) => {
+    if (loading) return "Loading...";
+
+    const data = distributionData?.find((item) => item.financial_year === financialYear);
+    return data?.distributed_mahalaxmi_kits ?? "No data available";
+  };
+
+
+
+  return (
+    <div className="dashboard-container">
+      <DirectorLeftNav
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        isMobile={isMobile}
+        isTablet={isTablet}
+      />
+      <div className="main-content-dash">
+        <DirectorHeader toggleSidebar={toggleSidebar} />
+
+        <Container fluid className="dashboard-box mt-3">
+       Mahila poshan demand distribution 
+
+        </Container>
+       
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default DirMahilaPoshanDemandSector;
